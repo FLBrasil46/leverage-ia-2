f846b22fbc39c6d8c8b28f93e9d20ad0e1da7528
FLBrasil46 <davibitcoin2019@gmail.com>
2025-06-15 03:12:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -58,6 +58,4 @@ def index():
     return html
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    print("Usando porta:", port)
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, port=5000)
